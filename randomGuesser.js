6e730aadcde211de4602c4bc6229055fde4deab0
Seyi0 <makanjuolaseyi560@gmail.com>
2023-10-15 15:09:40 +0100

    :construction:

--- randomGuesser.js
+++ randomGuesser.js
@@ -3,6 +3,19 @@ const prompt = require("prompt-sync")();
 const target = 10 + Math.round (Math.random() *90);
 
 let guesses = 0;
+console.log(target)
+// understand while loop
 
+while (true){
 const guess = Number (prompt("Guess the number (0-100): "));
-console.log(guess);
+// understand conditional statement.....
+if (guess > target ) {
+    console.log("Your guess is too high.");
+} else if (guess<target){
+    console.log("Your guess is too low.");
+} else{
+    console.log ("You guessed it!")
+    break
+    // break is used to terminate the code....
+}
+}
